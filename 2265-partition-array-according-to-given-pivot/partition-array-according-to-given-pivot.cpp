c9bbee4b70a47e2cc41db8bfe8be0f9398296656
Kunal Rathore <89546050+pylinc@users.noreply.github.com>
2025-03-03 19:15:49 +0530

    Time: 10 ms (35.38%) | Memory: 127.6 MB (87.50%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>  lessThan;
-        vector<int> equal;
-        vector<int> greaterThan;
-
-        vector<int> result;
+        vector<int> result(nums.size());
+        int countLess = 0;
+        int equal = 0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]<pivot){
+                countLess++;
+            }
+            else if(nums[i]==pivot){
+                equal++;
+            }
+        }
+        int j=countLess;
+        int k=countLess+equal;
+        int index = 0;
 
         for(int i =0;i<nums.size();i++){
             if(nums[i]<pivot){
-                lessThan.push_back(nums[i]);
+                result[index++] = nums[i];
             }
-            else if(nums[i]>pivot){
-                greaterThan.push_back(nums[i]);
+            else if(nums[i]==pivot){
+                result[j] = nums[i];
+                j++;
             }
             else{
-                equal.push_back(nums[i]);
+                result[k] = nums[i];
+                k++;
             }
-        }
-        for(int i =0;i<lessThan.size();i++){
-            result.push_back(lessThan[i]);
-        }
-        for(int i =0;i<equal.size();i++){
-            result.push_back(equal[i]);
-        }
-        for(int i =0;i<greaterThan.size();i++){
-            result.push_back(greaterThan[i]);
         }
 
         return result;
+
+            
+        
     }
 };
